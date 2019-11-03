13724b6264b0516f2a2838fd2a79dd43d73ed64f
Karl <karlowich@gmail.com>
2019-11-03 21:33:09 +0100

    Made editWindow dynamic

--- src/editWindow.js
+++ src/editWindow.js
@@ -4,27 +4,64 @@ const { ipcRenderer } = electron;
 const form = document.querySelector('form');
 form.addEventListener('submit', submitForm);
 
-// Fill form with data from row
+const columns = ['composer', 'title', 'year'];
+
+//Load form
+document.addEventListener('DOMContentLoaded', function() {
+	let inputs = document.querySelector('#inputs');
+	for (let i = 0; i < columns.length; i++) {
+		const div = document.createElement('div');
+		div.className = 'input-field';
+		div.appendChild(createLabel(columns[i]));
+
+		// Make sure only first input is autofocused
+		if (i === 0) {
+			div.appendChild(createInput(columns[i], true));
+		} else {
+			div.appendChild(createInput(columns[i], false));
+		}
+
+		inputs.append(div);
+	}
+});
+
+function createLabel(column) {
+	this[column] = document.createElement('label');
+	this[column].className = 'active';
+	this[column].for = column;
+	// make sure first letter is capital
+	this[column].append(column.charAt(0).toUpperCase() + column.slice(1));
+	return this[column];
+}
+
+function createInput(column, autofocus) {
+	this[column] = document.createElement('input');
+	this[column].id = column;
+	this[column].type = 'text';
+	this[column].required = true;
+	this[column].autofocus = autofocus;
+	this[column].placeholder = '';
+	return this[column];
+}
+
+// Fill form with data
 ipcRenderer.on('item:edit', function(e, item) {
-	document.querySelector('#composer').value = item.composer;
-	document.querySelector('#title').value = item.title;
-	document.querySelector('#year').value = item.year;
+	for (let i = 0; i < columns.length; i++) {
+		document.querySelector(`#${columns[i]}`).value = item[columns[i]];
+	}
 });
 
 function submitForm(e) {
 	e.preventDefault();
 	const item = {
-		composer: formValue('#composer'),
-		tag: `${formValue('#composer')} ${formValue('#title')}`,
-		title: formValue('#title'),
-		year: formValue('#year')
+		tag: `${document.querySelector(`#${columns[0]}`).value} ${
+			document.querySelector(`#${columns[1]}`).value
+		}`
 	};
-
+	for (let i = 0; i < columns.length; i++) {
+		item[columns[i]] = document.querySelector(`#${columns[i]}`).value;
+	}
 	ipcRenderer.send('item:edit', item);
-}
-
-function formValue(id) {
-	return document.querySelector(id).value;
 }
 
 function closeEditWindow() {
